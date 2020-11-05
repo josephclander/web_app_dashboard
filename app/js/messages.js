ae23acd80e8d86e0136afcb0cd749bbf6daeaa3c
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-05 18:00:09 +0000

    Update message form submit to include error msg

--- app/js/messages.js
+++ app/js/messages.js
@@ -1,15 +1,30 @@
 // Message widget scripts
 
-const sendButton = document.querySelector('.messages__btn');
 const messageBox = document.querySelector('#messages__alert-window');
+const userSearch = document.querySelector('#messages__search');
+// const sendButton = document.querySelector('.messages__btn');
+const messageForm = document.querySelector('#messagesForm');
 
 const submitMessageHandler = (event) => {
   event.preventDefault();
-  messageBox.innerText = 'Message Sent';
+  const info = messageForm.elements;
+  let userNameInput = info[0].value;
+  let userMessageInput = info[1].value;
+
+  if (userNameInput.length === 0 || userMessageInput.length === 0) {
+    messageBox.innerText = 'Please enter name and message';
+  } else {
+    messageBox.innerText = 'Message Sent';
+    messageForm.reset();
+    // console.log(userNameInput, userMessageInput);
+  }
+
   messageBox.classList.add('messages__alert-window--show');
   setInterval(() => {
     messageBox.classList.remove('messages__alert-window--show');
   }, 2000);
 };
 
-sendButton.addEventListener('click', submitMessageHandler);
+// sendButton.addEventListener('click', submitMessageHandler);
+
+messageForm.addEventListener('submit', submitMessageHandler);
