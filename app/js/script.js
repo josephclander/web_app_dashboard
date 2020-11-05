26185dcbdb4196e593ec9e90d991f4352b6f725e
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-05 13:43:23 +0000

    Add alert bar removal function

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,8 @@
-console.log('js is loaded');
+const alertBar = document.querySelector('.alert');
+const alertCross = document.querySelector('.alert__close');
+
+function alertMessageHandler() {
+  alertBar.style = 'display: none';
+}
+
+alertCross.addEventListener('click', alertMessageHandler);
