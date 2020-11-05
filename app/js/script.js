d10622cf1f91ae0e51f8c729269a72772214486a
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-05 15:25:25 +0000

    Update Gulp for es6, add notes dropdown

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,33 @@
 const alertBar = document.querySelector('.alert');
 const alertCross = document.querySelector('.alert__close');
 
+const bell = document.querySelector('.user__notification');
+const notificationMessageList = document.querySelectorAll(
+  '.notifications__item'
+);
+const notificationCrosses = document.querySelectorAll('.notifications__close');
+
+// Alert bar
 function alertMessageHandler() {
   alertBar.style = 'display: none';
 }
 
 alertCross.addEventListener('click', alertMessageHandler);
+
+// Notifications bell
+const openMessageHandler = () => {
+  notificationMessageList.forEach((item) => {
+    item.style = 'display: flex';
+  });
+};
+
+bell.addEventListener('click', openMessageHandler);
+
+const notificationsMessageHandler = (event) => {
+  const parent = event.target.parentNode;
+  parent.style = 'display: none';
+};
+
+notificationCrosses.forEach((cross) => {
+  return cross.addEventListener('click', notificationsMessageHandler);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -40,7 +40,7 @@ function jsTask() {
     //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
   ])
     .pipe(concat('all.js'))
-    .pipe(uglify())
+    .pipe(terser())
     .pipe(dest('dist'));
 }
 
