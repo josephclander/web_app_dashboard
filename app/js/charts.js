51c8b9afb1b1ab5ec65b7c2d34affda3b2a92064
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-09 21:37:08 +0000

    Add line-chart and time options w styles

--- app/js/charts.js
+++ app/js/charts.js
@@ -1,5 +1,6 @@
 var barElement = document.getElementById('bar-chart');
 var donutElement = document.getElementById('donut-chart');
+var lineElement = document.getElementById('line-chart');
 
 // Chart.defaults.global.legend.display = false;
 Chart.defaults.global.tooltips.enabled = false;
@@ -81,6 +82,66 @@ var donutChart = new Chart(donutElement, {
         fontFamily: "'Open Sans', sans-serif",
         fontColor: '#686767',
       },
+    },
+  },
+});
+
+var lineChart = new Chart(lineElement, {
+  type: 'line',
+  data: {
+    labels: [
+      'week 5',
+      'week 6',
+      'week 7',
+      'week 8',
+      'week 9',
+      'week 10',
+      'week 11',
+      'week 12',
+      'week 13',
+      'week 14',
+    ],
+    datasets: [
+      {
+        data: [500, 1200, 800, 1000, 1350, 1100, 1000, 1500, 1250, 1400],
+        lineTension: 0,
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderColor: 'rgb(116, 119, 191)',
+        borderWidth: 1,
+        pointBackgroundColor: '#fff',
+        pointBorderColor: 'rgb(116, 119, 191)',
+      },
+    ],
+  },
+  options: {
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            fontColor: '#b2b2b2',
+            padding: 15,
+          },
+          gridLines: {
+            drawTicks: false,
+            zeroLineColor: 'rgba(0, 0, 0, 0.1)',
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            fontColor: '#b2b2b2',
+            padding: 10,
+          },
+          gridLines: {
+            drawTicks: false,
+            zeroLineColor: 'rgba(0, 0, 0, 0.1)',
+          },
+        },
+      ],
     },
   },
 });
