12a9e0f776154722670ce6427c1842f681ebaa7c
Joseph Lander <josephclander@gmail.com>
2023-01-22 08:50:52 +0000

    Update Gulp build and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
 // Initialize modules
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
-const { src, dest, watch, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require("gulp");
 // Importing all the Gulp-related packages we want to use
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const terser = require('gulp-terser');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const replace = require('gulp-replace');
-const browserSync = require('browser-sync');
-const svgo = require('gulp-svgo');
-const svgstore = require('gulp-svgstore');
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const replace = require("gulp-replace");
+const browserSync = require("browser-sync");
+const svgo = require("gulp-svgo");
+const svgstore = require("gulp-svgstore");
 
 const server = browserSync.create();
 
 // File paths
 const files = {
-  indexPath: 'app/*.html',
-  scssPath: 'app/scss/**/*.scss',
-  jsPath: 'app/js/**/*.js',
+  indexPath: "app/*.html",
+  scssPath: "app/scss/**/*.scss",
+  jsPath: "app/js/**/*.js",
 };
 
 // Sass task: compiles the style.scss file into style.css
@@ -29,8 +29,8 @@ function scssTask() {
     .pipe(sourcemaps.init()) // initialize sourcemaps first
     .pipe(sass()) // compile SCSS to CSS
     .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
-    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-    .pipe(dest('dist')); // put final CSS in dist folder
+    .pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
+    .pipe(dest("docs")); // put final CSS in dist folder
 }
 
 // JS task: concatenates and uglifies JS files to script.js
@@ -39,37 +39,37 @@ function jsTask() {
     files.jsPath,
     //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
   ])
-    .pipe(concat('all.js'))
+    .pipe(concat("all.js"))
     .pipe(terser())
-    .pipe(dest('dist'));
+    .pipe(dest("docs"));
 }
 
 // Cachebust
 function cacheBustTask() {
   var cbString = new Date().getTime();
-  return src(['./dist/*.html'])
-    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-    .pipe(dest('dist'));
+  return src(["./dist/*.html"])
+    .pipe(replace(/cb=\d+/g, "cb=" + cbString))
+    .pipe(dest("docs"));
 }
 
 // Copy Images task: copies images folder to dist
 function copyImagesTask() {
-  return src(['app/images/**/*']).pipe(dest('dist/images'));
+  return src(["app/images/**/*"]).pipe(dest("docs/images"));
 }
 
 // Copy Icons task: optimize icons folder to dist
 function copyIconsTask() {
-  return src(['app/icons/**/*']).pipe(svgo()).pipe(dest('app/icons'));
+  return src(["app/icons/**/*"]).pipe(svgo()).pipe(dest("app/icons"));
 }
 
 // Create svg sprite
 function iconSpriteTask() {
-  return src('app/icons/*.svg').pipe(svgstore()).pipe(dest('dist'));
+  return src("app/icons/*.svg").pipe(svgstore()).pipe(dest("docs"));
 }
 
 // Copy Index task: copies index file to dist
 function copyIndexTask() {
-  return src(['app/*.html']).pipe(dest('dist'));
+  return src(["app/*.html"]).pipe(dest("docs"));
 }
 
 // Browser-sync local server
@@ -81,7 +81,7 @@ function reload(done) {
 function serve(done) {
   server.init({
     server: {
-      baseDir: './dist',
+      baseDir: "./docs",
     },
   });
   done();
